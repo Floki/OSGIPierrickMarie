d7e414884ed92cd4cb6bfaf880ae407a3182d24e
VincentCarassus <vincentcarassus@gmail.com>
2013-11-15 17:08:56 +0100

    More weathers possibilities

--- org.ups.weather/src/org/ups/weather/WeatherType.java
+++ org.ups.weather/src/org/ups/weather/WeatherType.java
@@ -2,4 +2,5 @@ package org.ups.weather;
 
 public enum WeatherType {
 	SHINY, CLOUDY, RAINY, SHOWERS, SNOW, UNKNOWN
+	// , METEOR_STORM, FROG_RAIN, STONE_RAIN, DROUGH, MONSOON, TSUNAMI, TYPHON, CHUCK_NORRIS
 }
