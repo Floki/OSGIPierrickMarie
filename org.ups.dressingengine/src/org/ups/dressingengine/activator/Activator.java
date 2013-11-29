2c1e81c5d9246a1fea42d9b6b7e7907b58bd6659
VincentCarassus <vincentcarassus@gmail.com>
2013-11-29 16:11:45 +0100

    Update DressingSuggestion
    blablabla

--- org.ups.dressingengine/src/org/ups/dressingengine/activator/Activator.java
+++ org.ups.dressingengine/src/org/ups/dressingengine/activator/Activator.java
@@ -3,8 +3,16 @@
  */
 package org.ups.dressingengine.activator;
 
+import java.util.Dictionary;
+import java.util.Hashtable;
+
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.ups.dressingengine.IDressingSuggestion;
+import org.ups.dressingengine.impl.DressingSuggestionImpl;
+import org.ups.weather.IWeather;
+import org.ups.weather.IWeatherListener;
 
 
 
@@ -12,21 +20,36 @@ import org.osgi.framework.BundleContext;
  *
  */
 public class Activator implements BundleActivator {
+	
+	private IDressingSuggestion service = null;
+	
+	public Activator() {
+		this.service = new DressingSuggestionImpl();
+	}
 
 	/* (non-javadoc)
 	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
 	 */
 	public void start(BundleContext context) throws Exception {
-		// TODO Auto-generated method stub
+		System.out.println("OSGi DRESSING_SUGGESTION : org.ups.dressingengine.Activator.start()");
 		
+		System.out.println("OSGi DRESSING_SUGGESTION : Connection to the Location module");
+		ServiceReference<?>[] references = context.getServiceReferences(IWeather.class.getName(), "(name=*)");
+
+		for (ServiceReference<?> reference : references) {
+			((IWeather) context.getService(reference)).addListener((IWeatherListener) this.service);
+		}
+		
+		Dictionary<String, String> properties = new Hashtable<String, String>();
+		properties.put("name", "org.ups.dressingsuggestion");
+		context.registerService(IDressingSuggestion.class.getName(), this.service, properties);
 	}
 
-	/* (non-javadoc)
-	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
-	 */
+	
 	public void stop(BundleContext context) throws Exception {
-		// TODO Auto-generated method stub
+		System.out.println("OSGi LOCATION: org.ups.dressingengine.Activator.start()");
 		
+		this.service = null;
 	}
 
 }
--- org.ups.dressingengine/src/org/ups/dressingengine/impl/DressingSuggestionImpl.java
+++ org.ups.dressingengine/src/org/ups/dressingengine/impl/DressingSuggestionImpl.java
@@ -8,58 +8,93 @@ import java.util.List;
 
 import org.ups.dressingengine.IDressingSuggestion;
 import org.ups.dressingengine.IDressingSuggestionListener;
+import org.ups.weather.IWeatherListener;
+import org.ups.weather.WeatherType;
 
 
 
 /**
  *
  */
-public class DressingSuggestionImpl implements IDressingSuggestion {
+public class DressingSuggestionImpl implements IDressingSuggestion, IWeatherListener {
 	
 	
+	private boolean coat = false;
+	private boolean sunglasses = false;
+	private boolean umbrella = false;
+
 	private List<IDressingSuggestionListener> listeners = new ArrayList<IDressingSuggestionListener>();
 
+	public boolean sunGlassesNeeded() {
+		return this.sunglasses;
+	}
 
-	/* (non-javadoc)
-	 * @see org.ups.dressingengine.IDressingSuggestion#addListener(org.ups.dressingengine.IDressingSuggestionListener)
-	 */
+
+	public boolean umbrellaNeeded() {
+		return this.umbrella;
+	}
+
+	public boolean coatNeeded() {
+		return this.coat;
+	}
+
+	
+
+
 	public void addListener(IDressingSuggestionListener listener) {
 		this.listeners.add(listener);
 	}
 
 
-	/* (non-javadoc)
-	 * @see org.ups.dressingengine.IDressingSuggestion#removeListener(org.ups.dressingengine.IDressingSuggestionListener)
-	 */
 	public void removeListener(IDressingSuggestionListener listener) {
 		this.listeners.remove(listener);
 	}
 
 
 	/* (non-javadoc)
-	 * @see org.ups.dressingengine.IDressingSuggestion#sunGlassesNeeded()
+	 * @see org.ups.weather.IWeatherListener#weatherChanged(org.ups.weather.WeatherType)
 	 */
-	public boolean sunGlassesNeeded() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-
-	/* (non-javadoc)
-	 * @see org.ups.dressingengine.IDressingSuggestion#umbrellaNeeded()
-	 */
-	public boolean umbrellaNeeded() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-
-	/* (non-javadoc)
-	 * @see org.ups.dressingengine.IDressingSuggestion#coatNeeded()
-	 */
-	public boolean coatNeeded() {
-		// TODO Auto-generated method stub
-		return false;
+	public void weatherChanged(WeatherType newWeather) {
+		switch(newWeather) {
+			case CLOUDY:
+				this.coat = false;
+				this.sunglasses = false;
+				this.umbrella = false;
+				break;
+			case RAINY:
+				this.coat = true;
+				this.sunglasses = false;
+				this.umbrella = true;
+				break;
+			case SHINY:
+				this.coat = false;
+				this.sunglasses = true;
+				this.umbrella = false;
+				break;
+			case SHOWERS:
+				this.coat = true;
+				this.sunglasses = false;
+				this.umbrella = true;
+				break;
+			case SNOW:
+				this.coat = true;
+				this.sunglasses = false;
+				this.umbrella = false;
+				break;
+			case UNKNOWN:
+				this.coat = false;
+				this.sunglasses = false;
+				this.umbrella = false;
+				break;
+			default:
+				this.coat = false;
+				this.sunglasses = false;
+				this.umbrella = false;
+				break;
+			
+		}
+		
+		
 	}
 
 }
